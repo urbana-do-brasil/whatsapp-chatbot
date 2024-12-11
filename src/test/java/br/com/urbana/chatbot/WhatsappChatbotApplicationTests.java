7866ed176838ba1e6288aac355f1fb4bd4e1ea05
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-12-11 19:06:02 -0300

    Commit com estrutura inicial da aplicação

---
+++ src/test/java/br/com/urbana/chatbot/WhatsappChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urbana.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
